fdf53e2b62c189aa6285a0bc469cceb875ecf9b7
Sergey_Prikhodko <sbprikhodko@yandex.ru>
2023-09-07 19:37:44 +0300

    update

---
+++ Basics/Branches_and_loops/Days_in_month/Solution/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+
+int main(){
+  int month, year;
+  std::cin >> month >> year;
+    
+  if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+  || month == 10 || month == 12){
+    std::cout << "31";
+  } else if (month != 2){
+    std::cout << "30";
+  } else if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0){
+    std::cout << "29";
+  } else{
+    std::cout << "28";
+  }
+
+  return 0;
+}
