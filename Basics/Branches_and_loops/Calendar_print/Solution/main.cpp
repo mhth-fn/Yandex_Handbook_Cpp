683ce152aa2ccbac10f22e9f049d71007fd0d323
Sergey_Prikhodko <sbprikhodko@yandex.ru>
2023-09-08 10:13:10 +0300

    update

--- Basics/Branches_and_loops/Calendar_print/Solution/main.cpp
+++ Basics/Branches_and_loops/Calendar_print/Solution/main.cpp
@@ -7,8 +7,8 @@ int main(){
   int current_day = 0;
   for (int i = 1; i < (first_day + days_in_month); i++){
 		if (i >= first_day){
-      current_day++;
-      if (current_day < 10 && i != 0){
+			current_day++;
+			if (current_day < 10 && i != 0){
 				std::cout << " ";
       }
 		std::cout << current_day;
